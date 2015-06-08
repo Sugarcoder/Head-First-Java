58f87c22912a8d1e62f0eca0d960b992b1aa00f7
Sugarcoder <mikichea@gmail.com>
2015-06-07 21:44:00 -0700

    networking notes

---
+++ Ch15ExDailyAdviceServer.java
@@ -0,0 +1,47 @@
+/* This program makes a ServerSocket and waits for client requests. 
+When it gets a client request (i.e. client said new Socket for this application), 
+the server makes a new Socket connection to that client. 
+The server makes a PrintWriter (using the Socket's output stream) and sends a message to the client. */
+
+
+package chap15;
+import java.io.*;
+import java.net.*;
+
+public class DailyAdviceServer
+{
+    String[] adviceList = {"Take smaller bites", "Go for the tight jeans. No they do NOT make you look fat",
+        "One word: inappropriate", "Just for today, be honest.  Tell your boss what you *really* think", 
+        "You might want to rethink that haircut"};
+        
+    public void go() {
+        try {
+            ServerSocket serverSock = new ServerSocket(4242);
+            while (true)
+            {
+                Socket sock = serverSock.accept();
+                
+                PrintWriter writer = new PrintWriter(sock.getOutputStream());
+                String advice = getAdvice();
+                writer.println(advice);
+                writer.close();
+                System.out.println(advice);
+            }
+        } catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+    
+    private String getAdvice() {
+        int random = (int) (Math.random() * adviceList.length);
+        return adviceList[random];
+    }
+    
+    public static void main(String[] args)
+    {
+        DailyAdviceServer server = new DailyAdviceServer();
+        server.go();
+    }
+
+}
